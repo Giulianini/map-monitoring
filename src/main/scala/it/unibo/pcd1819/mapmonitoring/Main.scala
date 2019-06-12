d77da320bae872aafc0132e076ab58b3615c5e35
Manuel Bonarrigo <manuel.bonarrigo@studio.unibo.it>
2019-06-12 11:55:40 +0200

    remove main

--- src/main/scala/it/unibo/pcd1819/mapmonitoring/Main.scala
+++
@@ -1,9 +0,0 @@
-package it.unibo.pcd1819.mapmonitoring
-
-import com.sun.javafx.application.PlatformImpl
-import it.unibo.pcd1819.mapmonitoring.view.screens.{MainScreenView, View}
-
-object Main extends App {
-  PlatformImpl.startup(() => {})
-  var view: View = MainScreenView()
-}
